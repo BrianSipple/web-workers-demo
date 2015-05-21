170cd8737158595a4ab666b0843e1f954bb96d7e
Brian Sipple <Bsipple57@gmail.com>
2015-05-21 12:01:14 -0700

    mathy web workers

--- js/main.js
+++ js/main.js
@@ -3,21 +3,31 @@
 if (window.Worker) {
 
 
-    var firstInput = document.querySelector('#firstInput'),
-        secondInput = document.querySelector('#secondInput');
+    var firstNumberInput = document.querySelector('#firstInputNum'),
+        secondNumberInput = document.querySelector('#secondInputNum'),
+        workerResult = document.querySelector('.worker-result.math');
+
 
     var mainWorker = new Worker('js/worker.js');
 
-    firstInput.onchange = function () {
-        mainWorker.postMessage([firstInput.value, secondInput.value]);
+    mainWorker.onmessage = function (e) {
+        workerResult.textContent = e.data;
+        console.log('Received result from worker');
+    };
+
+    firstNumberInput.onchange = function () {
+        mainWorker.postMessage([firstNumberInput.value, secondNumberInput.value]);
         console.log('Message posted to worker after change in input 1');
     };
 
 
-    secondInput.onchange = function () {
-        mainWorker.postMessage([firstInput.value, secondInput.value]);
+    secondNumberInput.onchange = function () {
+        mainWorker.postMessage([firstNumberInput.value, secondNumberInput.value]);
         console.log('Message posted to worker after change in input 2')
     };
+
+
+
 
 
 
--- js/worker.js
+++ js/worker.js
@@ -2,7 +2,7 @@
 
 onmessage = function (msg) {
     console.log('Message received from main script!');
-    var workerResult = 'Result: ' + (msg.data[0] * msg.data[1]);
+    var workerResult = 'Result: ' + (msg.data[0] || '0') + ' x ' + (msg.data[1] || '0') + ' is ' + (msg.data[0] * msg.data[1]);
 
     console.log('Posting message of ' + workerResult + ' back to main script');
     postMessage(workerResult);
